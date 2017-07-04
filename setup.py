41372fe4edbeff3d91e8d38a12992332eb424a0b
Daniel Patterson <dan24patt@gmail.com>
2017-07-04 21:49:00 +0100

    Use click and setuptools to make basic installable CLI

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+dependencies = ['click']
+
+setup(
+    name='ticket',
+    version='0.1.0',
+    url='https://github.com/dp28/ticket',
+    license='MIT',
+    author='Daniel Patterson',
+    author_email='dan24patt@gmail.com',
+    description='Command line tool to make it easier to work with git & pivotal tracker',
+    long_description=__doc__,
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=dependencies,
+    entry_points={
+        'console_scripts': [
+            'ticket = src.ticket:ticket',
+        ],
+    },
+)
--- src/ticket.py
+++ src/ticket.py
@@ -1,5 +1,12 @@
-def main():
-  print('Hello, world')
+import click
+
+@click.group()
+def ticket():
+    pass
+
+@ticket.command()
+def run():
+    click.echo('Hello, ticket')
 
 if __name__ == '__main__':
-  main()
+    ticket()
